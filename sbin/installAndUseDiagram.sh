c298fe985d866d03969102267f881b42f2e963c4
Noud de Brouwer <noud4@home.nl>
2020-11-03 08:55:48 +0100

    planning now

--- sbin/installAndUseDiagram.sh
+++ sbin/installAndUseDiagram.sh
@@ -5,4 +5,6 @@ composer require beyondcode/laravel-er-diagram-generator --dev
 php artisan vendor:publish --provider=BeyondCode\\ErdGenerator\\ErdGeneratorServiceProvider
 # use
 php artisan make:model Application
+# @todo place Application and User out Models, one up
+# @todo correct the so-far
 php artisan generate:erd
